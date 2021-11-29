40a31a414eae284e10fb099fe149bdca4faeb9d5
ekzamiralov <egor.zamiralov@gmail.com>
2021-11-29 20:55:54 +0300

    + add connect8 penetrations project;

---
+++ Embedded.Penetrations.Cn8/Addin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Bentley.MstnPlatformNET;
+
+using shared = Shared;
+
+namespace Embedded.Penetrations
+{
+[AddInAttribute(MdlTaskID = "Embedded.Penetrations.Cn")]
+public sealed class Addin : Bentley.MstnPlatformNET.AddIn
+{
+    private static Addin instance_;
+    public static Addin Instance
+    {
+       get { return instance_; }
+    }
+
+    protected override int Run(string[] commandLine)
+    {
+        shared.Logger.Log.Info("старт приложения");
+        return 0;
+    }
+
+    public Addin(IntPtr mdlDesc)
+        : base(mdlDesc)
+    {
+        instance_ = this;
+    }   
+}
+}
---
+++ Embedded.Penetrations.Cn8/Keyins.cs
@@ -0,0 +1,39 @@
+﻿using Shared;
+using Embedded.Penetrations.Shared;
+using Embedded.Penetrations.Shared.Mapping;
+using System;
+using System.Windows.Forms;
+using Shared.Bentley;
+
+namespace Embedded.Penetrations
+{
+static class Keyins
+{
+    public static void showForm(string unparsed)
+    {
+        try
+        {
+            PenetrationVM.getInstance(
+                Addin.Instance, new KeyinOptions(unparsed)).showForm();
+        }
+        catch (Exception ex)
+        {
+            Logger.Log.Error(ex);
+            ex.ShowMessageBox();
+        }
+    }
+
+    public static void convertTagsToDataGroup(string unparsed)
+    {
+        try
+        {
+            TagsToDataGroupConverter.Run();
+        }
+        catch (Exception ex)
+        {
+            Logger.Log.Error(ex);
+            ex.ShowMessageBox();
+        }
+    }
+}
+}
